61f510235cffaa9f66026fe8c5bf768808ae43e8
Rinawaticodes <kyurei.kim@gmail.com>
2023-03-28 17:06:40 +0200

    basic 3 react commit

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -1,8 +1,22 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const pokemon = pokemonList[1];
+
 function PokemonCards() {
-    return <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-      <figcaption>Bulbasur</figcaption>
-    </figure>;
+    return (
+    <figure>
+      {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt="" />}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>);
   }
   
   export default PokemonCards;
